3ccb925c167bb92cd9b621e69bf15c39c3af7a4d
Quinn <quinnodel@gmail.com>
2023-11-19 11:30:52 -0700

    formatting changes

--- Primes/primes.c
+++ Primes/primes.c
@@ -1,79 +1,71 @@
-/** -------------------------------
+/**
+ * ------------------------------------------------
  * Author: Quinn Hiaasen
- * Date Created: Nov 17 2023
------------------------------------*/
+ * Date Created: Nov 17, 2023
+ * ------------------------------------------------
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 
+// Refactor the listprimes into smaller parts
 
-// refactor the listprimes into smaller parts
-
-bool isPrimeBF(int n)
-{
+bool isPrimeBF(int n) {
     bool isPrime = true;
-    for (int j = n - 1; j > 1; j--)                                 
-    {
-        if (n % j == 0)                                             
-        {                                                           
-             isPrime = false;                                       
-         }
+    for (int j = n - 1; j > 1; j--) {
+        if (n % j == 0) {
+            isPrime = false;
+        }
     }
     return isPrime;
 }
 
-void printNPrimes(int number)
-{
-    int total_primes_found= 0;                                            
+void printNPrimes(int number) {
+    int total_primes_found = 0;
     bool quota_met = false;
-
     int i = 2;
-    while (quota_met == false)
-    {
+
+    while (!quota_met) {
         clock_t start, end;
         double time_elapsed;
 
         start = clock();
-
         bool is_prime = isPrimeBF(i);
-
         end = clock();
 
         time_elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-        if (is_prime)                                                
-        {                                                           
+        if (is_prime) {
             total_primes_found++;
-            printf("Prime: %d --- Time: %f sec --- Progress: %d/%d found\n", i, time_elapsed,total_primes_found,number);
+            printf("Prime: %d --- Time: %f sec --- Progress: %d/%d found\n", i, time_elapsed, total_primes_found, number);
         }
 
-        if (total_primes_found == number)
-        {
+        if (total_primes_found == number) {
             quota_met = true;
         }
-        i++;                                                        
+
+        i++;
     }
 }
 
-int main(int argc, char *argv[])                                        // argc = number of arguments entered
-{                                                                       // argv[] = index into each argument of string
-    if(atoi(argv[1])!=0)                                            // atoi() casts a string to an integer
-        {                                                               // returns 0 if it finds a char thats not a digit
-            clock_t start,end;
-            double total_time;
-            int n = atoi(argv[1]);
-            start = clock();
-            printNPrimes(n);
-            end = clock();
-            total_time =  ((double)(end-start)) / CLOCKS_PER_SEC;
-            printf("--- %d primes found in %f seconds. --- \n", n,total_time);
-        }
-        else
-        {
-            printf("Please enter an integer as argument.\n");
-            printf("Syntax: ./primes <numberofprimes>\n");
-        }
+int main(int argc, char *argv[]) {
+    if (atoi(argv[1]) != 0) {
+        clock_t start, end;
+        double total_time;
+        int n = atoi(argv[1]);
+
+        start = clock();
+        printNPrimes(n);
+        end = clock();
+
+        total_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("--- %d primes found in %f seconds. ---\n", n, total_time);
+    } else {
+        printf("Please enter an integer as argument.\n");
+        printf("Syntax: ./primes <numberofprimes>\n");
+    }
+
     return 0;
 }
--- Sorting/mysort.c
+++ Sorting/mysort.c
@@ -8,30 +8,35 @@ Date Created - 11/19/2023
 #include <stdbool.h>
 #include <time.h>
 
-void printIntArray(int* arr, int size)
-{
-    for(int i = 0; i < size; i++)
-    {
+int *mySort(int *rand_arr) {
+    // Sorting logic here
+}
+
+void printIntArray(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d\n", arr[i]);
     }
 }
 
-int* createRandArray(int size)
-{
-    srand(time(NULL));
-    int* rand_array = (int*)malloc(size * sizeof(int));
-    for(int i = 0; i < size; i++)
-    {
-       rand_array[i] = rand(); 
+int *createRandArray(int size) {
+    srand(time(NULL)); // sets the random seed
+    int *rand_array = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        rand_array[i] = rand();
     }
     return rand_array;
 }
 
-int main (int argc, char* argv[])
-{
+/*
+--- NOTES ---
+- Implement error handing for arguments.
+- implement sorting algorithm
+*/
+
+int main(int argc, char *argv[]) {
     int size = atoi(argv[1]);
-    int* rand_array = createRandArray(size);
-    printIntArray(rand_array,size);
+    int *rand_array = createRandArray(size);
+    printIntArray(rand_array, size);
     free(rand_array);
 
     return 0;
