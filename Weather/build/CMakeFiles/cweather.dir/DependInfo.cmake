ee3cef859acb1a05b0498be178452866eb668cfb
Quinn Hiaasen <quinnodel@gmail.com>
2023-11-27 09:27:01 -0700

    .gitignore exclude build

--- Weather/build/CMakeFiles/cweather.dir/DependInfo.cmake
+++ Weather/build/CMakeFiles/cweather.dir/DependInfo.cmake
@@ -8,12 +8,12 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/quinnodel/Documents/Code/C/Weather/src/cJSON.c" "CMakeFiles/cweather.dir/src/cJSON.c.o" "gcc" "CMakeFiles/cweather.dir/src/cJSON.c.o.d"
-  "/home/quinnodel/Documents/Code/C/Weather/src/weather.c" "CMakeFiles/cweather.dir/src/weather.c.o" "gcc" "CMakeFiles/cweather.dir/src/weather.c.o.d"
+  "/Users/quinnodel/Documents/Code/C/Weather/src/cJSON.c" "CMakeFiles/cweather.dir/src/cJSON.c.o" "gcc" "CMakeFiles/cweather.dir/src/cJSON.c.o.d"
+  "/Users/quinnodel/Documents/Code/C/Weather/src/weather.c" "CMakeFiles/cweather.dir/src/weather.c.o" "gcc" "CMakeFiles/cweather.dir/src/weather.c.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
