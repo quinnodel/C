cb855e98489dc44dcbaa3a7172c69f8dc0290730
Quinn <quinnodel@gmail.com>
2023-11-27 19:06:55 -0700

    all: configured cmake

---
+++ Sorting/BubbleSort/build/CMakeFiles/bubblesort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bubblesort.dir/src/bubblesort.c.o"
+  "CMakeFiles/bubblesort.dir/src/bubblesort.c.o.d"
+  "bubblesort"
+  "bubblesort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bubblesort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sorting/HeapSort/build/CMakeFiles/heapsort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heapsort.dir/src/heapsort.c.o"
+  "CMakeFiles/heapsort.dir/src/heapsort.c.o.d"
+  "heapsort"
+  "heapsort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/heapsort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
